512d88386f9ad61a7187e3cd55abca465739b591
AntonyCheng <1911261716@qq.com>
2026-03-20 09:12:41 +0800

    SpringBoot初始化模板v2.2.1 更新Mybatis-Plus

--- src/main/java/top/sharehome/springbootinittemplate/config/mybatisplus/MyBatisPlusConfiguration.java
+++ src/main/java/top/sharehome/springbootinittemplate/config/mybatisplus/MyBatisPlusConfiguration.java
@@ -1,6 +1,5 @@
 package top.sharehome.springbootinittemplate.config.mybatisplus;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.DataChangeRecorderInnerInterceptor;
